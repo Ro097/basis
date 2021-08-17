04dd228fa106e62952d550d5dcc8b92b3c6c3977
luojun <1983098920@qq.com>
2021-08-17 22:34:19 +0800

    innerclasses + BigEgg2

---
+++ scala/src/main/java/basis/innerclasses/BigEgg2.java
@@ -0,0 +1,55 @@
+package basis.innerclasses;
+
+/**
+ * 内部类继承测试
+ *
+ * @author luojun
+ * @create 2021-08-17 22:17
+ */
+public class BigEgg2 extends Egg2 {
+    public class Yolk extends Egg2.Yolk {
+        public Yolk() {
+            System.out.println("BigEgg2.Yolk()");
+        }
+
+        @Override
+        public void f() {
+            System.out.println("BigEgg2.Yolk.f()");
+        }
+    }
+
+    public BigEgg2() {
+        insertYolk(new Yolk());
+    }
+
+    public static void main(String[] args) {
+        BigEgg2 e2 = new BigEgg2();
+        e2.g();
+    }
+}
+
+class Egg2 {
+    protected class Yolk {
+        public Yolk() {
+            System.out.println("Egg2.Yolk()");
+        }
+
+        public void f() {
+            System.out.println("Egg2.Yolk.f()");
+        }
+    }
+
+    private Yolk y = new Yolk();
+
+    public Egg2() {
+        System.out.println("new Egg2()");
+    }
+
+    public void insertYolk(Yolk yy) {
+        y = yy;
+    }
+
+    public void g() {
+        y.f();
+    }
+}
